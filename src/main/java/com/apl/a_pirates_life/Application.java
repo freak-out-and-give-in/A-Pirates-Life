c1f853b47440a4ad79541dde595f8764f2af78d1
James <James10384@outlook.com>
2025-08-01 03:17:18 +0100

    Adding the initial files

---
+++ src/main/java/com/apl/a_pirates_life/Application.java
@@ -0,0 +1,13 @@
+package com.apl.a_pirates_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/apl/a_pirates_life/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.apl.a_pirates_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
